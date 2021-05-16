b0506c5511023b22d55bb282039b6d1f32158184
Vedat ERMIS <vedatermis@hotmail.com>
2021-05-16 12:39:02 +0300

    commit

--- src/pages/shop/shop.component.js
+++ src/pages/shop/shop.component.js
@@ -1,5 +1,6 @@
 import React from "react";
 import SHOP_DATA from "./shop.data";
+import CollectionPreview from "../../components/preview-collection/collection-preview.component";
 
 class ShopPage extends React.Component {
     constructor(props) {
@@ -11,9 +12,15 @@ class ShopPage extends React.Component {
     }
 
     render() {
+        const { collections } = this.state;
+
         return (
-            <div>
-                Shop Page
+            <div className="shop-page">
+                {
+                    collections.map(({id, ...otherCollectionProps }) => (
+                        <CollectionPreview key={id} {...otherCollectionProps} />
+                    ))
+                }
             </div>
         )
     }
