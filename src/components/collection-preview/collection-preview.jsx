e271eda118f105bdef9fcd88e1bdb66ec37608ad
Vedat ERMIS <vedatermis@hotmail.com>
2021-05-16 17:20:09 +0300

    c

--- src/components/collection-preview/collection-preview.jsx
+++ src/components/collection-preview/collection-preview.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import CollectionItem from "../collection-item/collection-item.component";
 import "./collection-preview.component.styles.scss";
 
 const CollectionPreview = ({ title, items}) => (
@@ -7,8 +8,8 @@ const CollectionPreview = ({ title, items}) => (
         <h1 className="title">{title.toUpperCase()}</h1>
         <div className="preview">
             {
-                items.filter((item, idx) => idx < 4).map((item) => (
-                    <div key={item.id}>{item.name}</div>
+                items.filter((item, idx) => idx < 4).map(({id, ...itemProps}) => (
+                    <CollectionItem key={id} {...itemProps} />
                 ))
             }
         </div>
